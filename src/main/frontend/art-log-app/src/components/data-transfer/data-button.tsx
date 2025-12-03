2a981f6472715891c7926a1cd7d3dd56b60bb98f
Jolie Zhu <zhujolie973@gmail.com>
2025-12-02 20:42:50 -0500

    student data transfer feature complete. will add attendance data transfer and try/catch later.

--- src/main/frontend/art-log-app/src/components/data-transfer/data-button.tsx
+++ src/main/frontend/art-log-app/src/components/data-transfer/data-button.tsx
@@ -1,7 +1,11 @@
 // External imports
+import React from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod" // Used for input validation
+
+// Internal imports
+import { add } from "@/restAPI/entities"
 
 // UI imports
 import { Button } from "../ui/button"
@@ -24,33 +28,58 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
+import { toast } from "sonner"
 
 // Schema with expected input types and error messages if input is invalid
 const dataSchema = z.object({
-    data: z.string({
+    student_data: z.string({
         message: "Data is required.",
     }),
+    attendance_data: z.string({
+        message: "Data is required."
+    })
 })
 
 // consider when empty (just don't update anything)
 
 export default function DataButton() {
 
+    // variable initializations
+    const studentUrl = "http://localhost:8080/student/"
+    const [open, setOpen] = React.useState(false)
+
     // Define form
     const form = useForm<z.infer<typeof dataSchema>>({
         resolver: zodResolver(dataSchema),
         defaultValues: {
-            data: "",
+            student_data: "",
+            attendance_data: "",
         },
     })
 
     async function onSubmit(values: z.infer<typeof dataSchema>) {
-        console.log(values);
+        console.log(values.student_data);
+        const studentData = values.student_data;
+        const jsonList = studentData.split("\n");
+        console.log(jsonList)
+
+        jsonList.forEach(async (jsonString: string) => {
+            console.log(jsonString)
+            const jsonBody = JSON.parse(jsonString);
+            console.log(jsonBody)
+            await add(studentUrl, jsonBody);
+        })
+        setOpen(false);
+        form.reset();
+        toast(`All data has been added. Please refresh the page.`);
+
+        // later: add a try catch block to ensure data given is in JSON form
+        // later: add attendance data transfer
     }
 
     return (
         <>
-            <Dialog>
+            <Dialog open={open} onOpenChange={setOpen}>
                 <DialogTrigger asChild>
                     <Button>Add Data</Button>
                 </DialogTrigger>
@@ -62,16 +91,33 @@ export default function DataButton() {
                     </DialogDescription>
                     </DialogHeader>
                     <Form {...form}>
-                        <form onSubmit={form.handleSubmit(onSubmit)} >
+                        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                             <FormField
                                 control={form.control}
-                                name="data"
+                                name="student_data"
                                 render={({ field }) => (
                                     <FormItem>
-                                        <FormLabel />
+                                        <FormLabel>Student Data</FormLabel>
                                         <FormControl>
                                             <Textarea 
-                                                placeholder="Place your JSON data here"
+                                                placeholder="Place your JSON student data here."
+                                                value={String(field.value)} 
+                                                onChange={field.onChange} 
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="attendance_data"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Attendance Data</FormLabel>
+                                        <FormControl>
+                                            <Textarea 
+                                                placeholder="Place your JSON attendance data here."
                                                 value={String(field.value)} 
                                                 onChange={field.onChange} 
                                             />
@@ -93,20 +139,3 @@ export default function DataButton() {
         </>
     )
 };
-
-/*
-#STUDENT
-{
-"first_name": "Jolie",
-"last_name": "Zhu",
-"class_id": "INTE-1",
-"day": "Monday",
-"phone_number": "1234567890",
-"time_expected": "12:00:00",
-"general_notes": "",
-"payment_number": 0,
-"class_number": 0,
-"total_classes": 0,
-"class_hours": 2,
-}
-*/
